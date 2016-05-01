18848543eb36c116e5afb454504d4b2beebe1baa
emily404 <emily-404@hotmail.com>
2016-05-02 00:07:39 +0200

    add dependency to package.json, synchronize screen and remote

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'));
+
+io.on('connection', function(socket){
+  console.log('a user connected ' + socket.id);
+
+  socket.on('image selection', function(index){
+  	io.emit('image selection', index);
+  	console.log('index: ' + index);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 http.listen(8080, function(){
     console.log('listening on *:8080');
